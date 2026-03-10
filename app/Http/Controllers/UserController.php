19b42a8cdd71352358339ad8ef4fbc011d8cfeb0
sileysa <sileysafaedatulnuraini984@gmail.com>
2026-03-10 10:50:16 +0700

    prak1

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -12,9 +12,12 @@ class UserController extends Controller
     {
         // tambah data user dengan Eloquent Model
         $data = [
-            'nama' => 'Pelanggan Pertama',
+            'level_id' => 2,
+            'username' => 'manager_tiga',
+            'nama' => 'Manager 3',
+            'password' => Hash::make('12345') // hash password sebelum disimpan
         ];
-        UserModel::where('username', 'customer-1')->update($data); // update data user
+        UserModel::create($data); // tambah data user
 
         // coba akses model UserModel
         $user = UserModel::all(); // ambil semua data dari tabel m_user
