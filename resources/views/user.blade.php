c85f6ed5c7fc903b91f3184562095295b73c4da3
sileysa <sileysafaedatulnuraini984@gmail.com>
2026-03-11 15:39:05 +0700

    prak2.3

--- resources/views/user.blade.php
+++ resources/views/user.blade.php
@@ -5,19 +5,15 @@
     </head>
     <body>
         <h1>Data User</h1>
-        <table border="1" cellpadding="2" cellspacing="0">
+        <table border="1">
             <tr>
-                <th>ID</th>
-                <th>Username</th>
-                <th>Nama</th>
-                <th>ID Level Pengguna</th>
+                <td>Jumlah Pengguna</td>
             </tr>
+
             <tr>
-                <td>{{ $data->user_id }}</td>
-                <td>{{ $data->username }}</td>
-                <td>{{ $data->nama }}</td>
-                <td>{{ $data->level_id }}</td>
+                <td>{{ $data }}</td>
             </tr>
-        </table>
+
+            </table>
     </body>
 </html>
