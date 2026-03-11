3a806cb2d958d0525e540726637b662442f72f9d
sileysa <sileysafaedatulnuraini984@gmail.com>
2026-03-11 23:30:47 +0700

    prak2.7

--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -4,13 +4,20 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\LevelModel;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class UserModel extends Model
 {
     use HasFactory;
 
-    protected $table = 'm_user';        //Mendefinisikan nama tabel yang digunakan oleh model ini
-    protected $primaryKey = 'user_id';  //Mendefinisikan primary key dari tabel yang digunakan
+    protected $table = 'm_user';       
+    protected $primaryKey = 'user_id';  
 
-    protected $fillable = ['level_id', 'username', 'nama', 'password']; //Mendefinisikan atribut yang dapat diisi secara massal
+    protected $fillable = ['level_id', 'username', 'nama', 'password']; 
+    
+    public function level(): BelongsTo
+    {
+        return $this->belongsTo(LevelModel::class, 'level_id', 'level_id');
+    }
 }
